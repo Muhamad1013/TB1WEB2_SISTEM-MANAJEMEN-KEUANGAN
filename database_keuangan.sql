234ed64dcec017636f6354cb601c8d87ba486e78
Muhamad Rafli <150500074+Muhamad1013@users.noreply.github.com>
2024-10-05 13:46:05 +0700

    Add files via upload

--- database_keuangan.sql
+++ database_keuangan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 04, 2024 at 08:11 PM
+-- Generation Time: Oct 05, 2024 at 08:45 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
